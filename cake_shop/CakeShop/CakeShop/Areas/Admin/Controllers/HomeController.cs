befb8bfca0122a0413339f84b7cbf3add8a702c2
Tôi Tên Du và Du Là Tôi <106806693+baolach123@users.noreply.github.com>
2024-02-17 14:30:43 +0700

    add project

---
+++ cake_shop/CakeShop/CakeShop/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CakeShop.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Admin")]
+ 
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ cake_shop/CakeShop/CakeShop/Models/TransactStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CakeShop.Models;
+
+public partial class TransactStatus
+{
+    public int TranSactStatusId { get; set; }
+
+    public string? Status { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
